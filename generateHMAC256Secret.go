54b1b39a9147fab869288f5331e10984916419ad
ABHINAV SUREKA <7731608+ABHINAV-SUREKA@users.noreply.github.com>
2022-05-26 19:14:06 +0530

    generateHMAC256Secret.go

--- generateHMAC256Secret.go
+++ generateHMAC256Secret.go
@@ -2,17 +2,38 @@ package go_utilities
 
 import (
 	"crypto/hmac"
+	"crypto/rand"
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/hex"
 )
 
+var (
+	secretKey = "some random secret key"
+	message   = "some random message"
+)
+
+// Generate a salt (random encoded string) with 16 bytes of crypto/rand data
+func generateSalt() string {
+	randomByteArr := make([]byte, 16)
+	_, err := rand.Read(randomByteArr)
+	if err != nil {
+		return ""
+	}
+	// encode the bytes using URLEncoding method of base64 encoding
+	return base64.URLEncoding.EncodeToString(randomByteArr)
+}
+
+// GenerateHMAC256Secret generates a keyed-hash i.e., a hash (using hash function such as SHA-2) that uses a secret key to sign a message
+// Since HMAC is not a cipher, it can't be decrypted
+// The recipient/server takes all the needed input, computes the HMAC using the secure secret key, and checks if the result is equal to the value in say, database
+// JWT also uses HMAC to encrypt JSON data/message
 func GenerateHMAC256Secret() string {
-	secret := "some random secret"
-	hmac := hmac.New(sha256.New, []byte(secret)) // create new HMAC by specifying hashing algo and a secret
+	hash := hmac.New(sha256.New, []byte(secretKey)) // create hash by specifying hashing algo and a secret
+	salt := generateSalt()
 
-	data := "some random data"
-	hmac.Write([]byte(data)) // write data into HMAC
+	hash.Write([]byte(message + salt)) // sign message + salt with the 'secretKey'
 
-	sha := hex.EncodeToString(hmac.Sum(nil)) // encode the result as hexadecimal string
+	sha := hex.EncodeToString(hash.Sum(nil)) // encode the result as hexadecimal string
 	return sha
 }
